9968aef3d1ce684a171d14ad6dbf1d3f8e06edfc
Kerollos Magdy <kerolloz@yahoo.com>
2020-04-26 01:31:48 +0200

    add health check endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import { HttpException, NOT_FOUND } from '../core/exceptions';
 import users from './users.route';
+import { endpoint } from '../core/decorators';
 
 const router = express.Router();
 
 router.use('/users', users);
+
+// healthcheck
+router.get(
+  '/ping',
+  endpoint((_) => 'pong'),
+);
 
 // 404
 router.all('*', () => {
