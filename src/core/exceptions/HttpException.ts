0c4e93bae080c524ab92928c3bdffb8931f868c1
Kerollos Magdy <kerolloz@yahoo.com>
2023-03-21 10:55:10 +0200

    add 409 status code to http exceptions

--- src/core/exceptions/HttpException.ts
+++ src/core/exceptions/HttpException.ts
@@ -1,6 +1,6 @@
 import { IErrorResponse } from '../decorators';
 
-export type HttpExceptionStatus = 400 | 401 | 403 | 404 | 422 | 500;
+export type HttpExceptionStatus = 400 | 401 | 403 | 404 | 409 | 422 | 500;
 
 export type ExceptionDetails = { message?: string; errors?: IErrorResponse[] };
 
--- src/core/exceptions/consts.ts
+++ src/core/exceptions/consts.ts
@@ -4,5 +4,6 @@ export const BAD_REQUEST: HttpExceptionStatus = 400;
 export const UNAUTHORIZED: HttpExceptionStatus = 401;
 export const FORBIDDEN: HttpExceptionStatus = 403;
 export const NOT_FOUND: HttpExceptionStatus = 404;
+export const CONFLICT: HttpExceptionStatus = 409;
 export const UNPROCESSABLE_ENTITY: HttpExceptionStatus = 422;
 export const SERVER_ERROR: HttpExceptionStatus = 500;
