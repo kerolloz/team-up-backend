ef1e4653acb8aaedc448547af9e8f4fdbea4e466
Kerollos Magdy <kerolloz@yahoo.com>
2023-03-23 13:52:10 +0200

    change test description

--- test/ping.test.ts
+++ test/ping.test.ts
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('GET /ping', () => {
-  it('returns a 200 with {"message": "pong"}', async () => {
+  it('returns 200 with {"message": "pong"}', async () => {
     const res = await chai.request(app).get('/ping');
     expect(res.status).to.equal(200);
     expect(res.body.message).to.equal('pong');
