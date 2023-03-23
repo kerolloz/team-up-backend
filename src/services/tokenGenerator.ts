0480ab25fd411ce98c361205abf3358225628ad1
Kerollos Magdy <kerolloz@yahoo.com>
2023-03-23 13:55:40 +0200

    fix outdated comment in token generator

--- src/services/tokenGenerator.ts
+++ src/services/tokenGenerator.ts
@@ -2,7 +2,7 @@ import { randomBytes } from 'crypto';
 
 /**
  * returns a random hex string
- * @param tokenLength default: 60
+ * @param tokenLength default: 16
  */
 export function generateToken(tokenLength = 16): string {
   return randomBytes(tokenLength).toString('hex');
