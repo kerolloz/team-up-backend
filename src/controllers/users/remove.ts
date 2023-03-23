fdafce176c6c7476d087aefa970a50bc47f056db
Kerollos Magdy <kerolloz@yahoo.com>
2023-03-23 18:32:45 +0200

    refactor DELETE /users/:token

--- src/controllers/users/remove.ts
+++ src/controllers/users/remove.ts
@@ -1,26 +1,22 @@
 import { Request } from 'express';
 import Joi from 'joi';
+import mongoose from 'mongoose';
 import { endpoint, SuccessfulResponse } from '../../core/decorators';
 import { BAD_REQUEST, HttpException } from '../../core/exceptions';
 import { UserModel } from '../../models/user';
 
 export default endpoint(
-  {
-    params: {
-      token: Joi.string().required(),
-    },
-  },
+  { params: { token: Joi.string().required() } },
   async (req: Request): Promise<SuccessfulResponse> => {
     const verificationToken = req.params.token;
-    const user = await UserModel.findOne({ verificationToken });
-
-    if (!user) {
-      throw new HttpException(BAD_REQUEST, {
-        message: 'Invalid token',
+    await UserModel.findOneAndRemove({ verificationToken })
+      .orFail()
+      .catch((err) => {
+        if (err instanceof mongoose.Error.DocumentNotFoundError) {
+          throw new HttpException(BAD_REQUEST, {
+            message: 'Invalid verification token',
+          });
+        }
       });
-    }
-
-    await user.remove();
-    return { status: 204 };
   },
 );
